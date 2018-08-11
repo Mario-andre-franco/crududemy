8e8c04da0c61bd36c78ae9b758bc7c55be5eb956
Mario Andre <mario.andre.franco@gmail.com>
2018-08-11 10:40:49 -0300

    Validacao de email

--- src/main/java/com/example/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/example/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.cursomc.domain.Cliente;
 import com.example.cursomc.domain.enums.TipoCliente;
 import com.example.cursomc.dto.ClienteNewDTO;
+import com.example.cursomc.repositories.ClienteRepository;
 import com.example.cursomc.resources.exeception.FieldMessage;
 import com.example.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	
+	@Autowired
+	
+	private ClienteRepository repo;
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,7 +37,13 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		
 		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
 		
+		if (aux != null) {
+			
+			list.add(new FieldMessage("email","Email já existente"));
+			
+		}
 		
 		
 
