9795de0e4fc79b3a95eead59aaf436c73c3260e8
Mario Andre <mario.andre.franco@gmail.com>
2018-08-11 20:36:21 -0300

    criando profile de dev

--- src/main/java/com/example/cursomc/config/DevConfi.java
+++ src/main/java/com/example/cursomc/config/DevConfi.java
@@ -3,6 +3,7 @@ package com.example.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,14 +12,20 @@ import com.example.cursomc.services.DBService;
 
 @Configuration
 @Profile("dev")
-public class TestConfig {
+public class DevConfi {
 
 	@Autowired
 	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
 
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
 
+		if(!"create".equals(strategy)) {
+			return false;
+		}
 		dbService.instantiateDateBase();
 
 		return true;
